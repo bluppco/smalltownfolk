e5b0e546b70acd4f5ef7c87abb4d6242df5f1aec
Mohit <mohit@hey.com>
2023-08-12 17:07:49 +0530

    x

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,10 @@ module.exports = {
 			  'clarendon': ["clarendon-text-pro", "serif"],
 			  'system': ["ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
 			},
-		},
+			colors: {
+				"primary": "#F0E15C"
+			}
+		}
 	},
 	plugins: [],
 }
